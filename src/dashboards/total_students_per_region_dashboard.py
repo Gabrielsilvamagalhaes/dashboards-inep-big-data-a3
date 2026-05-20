c17d44aa6d87777cbe7e323fcebce020f050bf4c
Gabriel Silva Magalhães <gabiles278@gmail.com>
2026-05-20 15:37:55 -0300

    refactor: limpar codigo da pagina de visão nacional

--- src/dashboards/total_students_per_region_dashboard.py
+++ src/dashboards/total_students_per_region_dashboard.py
@@ -3,16 +3,19 @@ from plotly.graph_objs import Figure
 import plotly.express as px
 
 
-def getTotalStudentsPerRegionChart(df_education: DataFrame, select_regions: list) -> Figure:
-   """ Função que retorna o grafico da distribuição do total de alunos por região"""
+def getTotalStudentsPerRegionChart(df: DataFrame, select_regions: list) -> Figure:
+    """Função que retorna o grafico da distribuição do total de alunos por região"""
+    df_education = df["NO_REGIAO"]
 
-   total_region = df_education.value_counts().reset_index()
-   total_region.columns = ['Região', 'Total']
+    total_region = df_education.value_counts().reset_index()
+    total_region.columns = ["Região", "Total"]
 
-   total_region_filtered = total_region[total_region['Região'].isin(select_regions)]
-   total_students_per_region_graphic = px.pie(total_region_filtered, 
-      values='Total',
-      names='Região',
-      title='Distribuição de Alunos por Região (Censo 2024)')
+    total_region_filtered = total_region[total_region["Região"].isin(select_regions)]
+    total_students_per_region_graphic = px.pie(
+        total_region_filtered,
+        values="Total",
+        names="Região",
+        title="Distribuição de Alunos por Região (Censo 2024)",
+    )
 
-   return total_students_per_region_graphic
+    return total_students_per_region_graphic
--- src/pages/national_vision_page.py
+++ src/pages/national_vision_page.py
@@ -21,8 +21,8 @@ def nationalVisionPage(df: DataFrame):
     df_filtered_per_region = df[df["NO_REGIAO"] == region]
     st.dataframe(df_filtered_per_region)
 
-    df_education = df["NO_REGIAO"]
-    regions = sorted(df_education.unique())
+    df_regions = df["NO_REGIAO"]
+    regions = sorted(df_regions.unique())
 
     # Estilizando label do multiselect
     st.markdown(
@@ -35,43 +35,55 @@ def nationalVisionPage(df: DataFrame):
     )
     col1, col2 = st.columns(2)
 
-    total_students_per_region_graphic = getTotalStudentsPerRegionChart(
-        cast(DataFrame, df_education), select_regions
-    )
-    col1.plotly_chart(total_students_per_region_graphic, width="stretch", on_select="rerun")
+    with col1:
+        total_students_per_region_graphic = getTotalStudentsPerRegionChart(
+            cast(DataFrame, df), select_regions
+        )
+        st.plotly_chart(
+            total_students_per_region_graphic,
+        )
 
-    df_education = df[["NO_REGIAO", "NO_UF"]]
-    total_students_per_state_graphic = getTotalStudentsPerStateChart(
-        cast(DataFrame, df_education), select_regions
-    )
-    col2.plotly_chart(total_students_per_state_graphic)
+    with col2:
+        total_students_per_state_graphic = getTotalStudentsPerStateChart(
+            cast(DataFrame, df), select_regions
+        )
+        st.plotly_chart(total_students_per_state_graphic)
 
     col3, col4, col5, col6 = st.columns(4)
 
     years = sorted(df["NU_ANO_CENSO"].dropna().astype(int).unique().tolist())
 
-    # Total de inscritos
-    total_students_registred_graphic = getTotalStudentsRegistredChart(df, years)
-    col3.plotly_chart(total_students_registred_graphic)
+    with col3:
+        # Total de inscritos
+        total_students_registred_graphic = getTotalStudentsRegistredChart(df, years)
+        st.plotly_chart(total_students_registred_graphic)
 
-    # Total Concluintes
-    total_students_finished_graphic = getTotalStudentsFinishedChart(df, years)
-    col4.plotly_chart(total_students_finished_graphic)
+    with col4:
+        # Total Concluintes
+        total_students_finished_graphic = getTotalStudentsFinishedChart(df, years)
+        st.plotly_chart(total_students_finished_graphic)
 
-    # Total ingressantes
-    total_students_new_entrants_graphic = getTotalStudentsNewEntrantsChart(df, years)
-    col5.plotly_chart(total_students_new_entrants_graphic)
+    with col5:
+        # Total ingressantes
+        total_students_new_entrants_graphic = getTotalStudentsNewEntrantsChart(df, years)
+        st.plotly_chart(total_students_new_entrants_graphic)
 
-    # Total de instituições
-    total_students_institutions_graphic = getTotalStudentsInstitutionsChart(df, years)
-    col6.plotly_chart(total_students_institutions_graphic)
+    with col6:
+        # Total de instituições
+        total_students_institutions_graphic = getTotalStudentsInstitutionsChart(df, years)
+        st.plotly_chart(total_students_institutions_graphic)
 
     col7, col8 = st.columns([1, 3])
 
-    # Total de cursos
     result = getTotalCoursesCharts(df)
     total_courses_indicator, total_courses_graphic = result.get(
         "total_courses_indicator"
     ), result.get("total_courses_graphic")
-    col7.plotly_chart(total_courses_indicator)
-    col8.plotly_chart(total_courses_graphic)
+
+    with col7:
+        # Indicador de total de cursos
+        st.plotly_chart(total_courses_indicator)
+
+    with col8:
+        # Top 20 cursos
+        st.plotly_chart(total_courses_graphic)
