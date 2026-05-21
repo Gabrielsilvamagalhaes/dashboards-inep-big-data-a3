baae110b737347a9136dfd487e4a5663306d1657
Hanspeter <hansdietiker2012@gmail.com>
2026-05-21 18:34:12 -0300

    feat<donut_dashboard_total_students>A donut control panel was created to display the total number of veterans, newcomers

--- src/pages/national_vision_page.py
+++ src/pages/national_vision_page.py
@@ -1,17 +1,14 @@
 from pandas import DataFrame
-import plotly.express as px
 import streamlit as st
 from typing import cast
 
 from dashboards.total_courses_dashboard import getTotalCoursesCharts
-from dashboards.total_students_finished_dashboard import getTotalStudentsFinishedChart
 from dashboards.total_students_institutions_dashboard import getTotalStudentsInstitutionsChart
-from dashboards.total_students_new_entrants_dashboard import getTotalStudentsNewEntrantsChart
 from dashboards.total_students_per_region_dashboard import (
     getTotalStudentsPerRegionChart,
 )
 from dashboards.total_students_per_state_dashboard import getTotalStudentsPerStateChart
-from dashboards.total_students_registred_dashboard import getTotalStudentsRegistredChart
+from dashboards.total_students_workflow_donut_dashboard import getTotalStudentsDonutChart
 
 
 def nationalVisionPage(df: DataFrame):
@@ -29,15 +26,14 @@ def nationalVisionPage(df: DataFrame):
     select_regions = st.multiselect(
         "Filtrar por regiões", regions, default=regions, label_visibility="hidden"
     )
+
     col1, col2 = st.columns(2)
 
     with col1:
         total_students_per_region_graphic = getTotalStudentsPerRegionChart(
             cast(DataFrame, df), select_regions
         )
-        st.plotly_chart(
-            total_students_per_region_graphic,
-        )
+        st.plotly_chart(total_students_per_region_graphic)
 
     with col2:
         total_students_per_state_graphic = getTotalStudentsPerStateChart(
@@ -45,41 +41,25 @@ def nationalVisionPage(df: DataFrame):
         )
         st.plotly_chart(total_students_per_state_graphic)
 
-    col3, col4, col5, col6 = st.columns(4)
-
-    years = sorted(df["NU_ANO_CENSO"].dropna().astype(int).unique().tolist())
+    col3, col4 = st.columns(2)
 
     with col3:
-        # Total de inscritos
-        total_students_registred_graphic = getTotalStudentsRegistredChart(df, years)
-        st.plotly_chart(total_students_registred_graphic)
-
-    with col4:
-        # Total Concluintes
-        total_students_finished_graphic = getTotalStudentsFinishedChart(df, years)
-        st.plotly_chart(total_students_finished_graphic)
-
-    with col5:
-        # Total ingressantes
-        total_students_new_entrants_graphic = getTotalStudentsNewEntrantsChart(df, years)
-        st.plotly_chart(total_students_new_entrants_graphic)
-
-    with col6:
-        # Total de instituições
-        total_students_institutions_graphic = getTotalStudentsInstitutionsChart(df, years)
+        total_students_institutions_graphic = getTotalStudentsInstitutionsChart(df)
         st.plotly_chart(total_students_institutions_graphic)
 
-    col7, col8 = st.columns([1, 3])
+    with col4:
+        total_students_workflow_donut = getTotalStudentsDonutChart(df)
+        st.plotly_chart(total_students_workflow_donut)
 
     result = getTotalCoursesCharts(df)
     total_courses_indicator, total_courses_graphic = result.get(
         "total_courses_indicator"
     ), result.get("total_courses_graphic")
 
-    with col7:
-        # Indicador de total de cursos
+    col5, col6 = st.columns([1, 3])
+
+    with col5:
         st.plotly_chart(total_courses_indicator)
 
-    with col8:
-        # Top 20 cursos
+    with col6:
         st.plotly_chart(total_courses_graphic)
